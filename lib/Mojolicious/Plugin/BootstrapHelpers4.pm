5b3f3bad51d90e25edc8407c3cc14efff4fef744
Erik Carlsson <info@code301.com>
2016-01-06 13:11:03 +0000

    Init

---
+++ lib/Mojolicious/Plugin/BootstrapHelpers4.pm
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+
+package Mojolicious::Plugin::BootstrapHelpers4;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Mojolicious::Plugin::BootstrapHelpers4;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::BootstrapHelpers4 is ...
+
+=head1 SEE ALSO
+
+=cut
